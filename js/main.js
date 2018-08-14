f700540263d573631520acbe904f1b74dda0db7e
Utah Kate Newman <utah.kate.newman@gmail.com>
2018-08-13 23:14:19 -0700

    add postBuilder, rude, and partial discomfort functions

--- js/main.js
+++ js/main.js
@@ -2,26 +2,68 @@ const therapist = document.querySelector('.therapist__pane'),
 	patient = document.querySelector('.patient__pane'),
 	patientInput = document.getElementById('chatInput'),
 	patientSubmit = document.getElementById('chatSubmit');
+var messageCount = 0;
+var keyStrokes = 0;
 
 (function greeting() {
 	therapist.innerHTML = '<p>Good Afternoon. How do you feel today?';
-	patient.innerHTML = '<p></p>';
 })();
 
+function preParse(message) {
+	messageCount++;
+	messageCount >= 7 ? patientInput.addEventListener('keyup', countKeysAndBeRude) : therapistResponse();
+}
+
+function countKeysAndBeRude() {
+	if (keyStrokes > 20) {
+		postBuilder(therapist, "I'm afraid that's all the time we have for today.");
+		patientInput.setAttribute("disabled", "true")
+	} else {
+		keyStrokes++;
+	}
+}
+
+function postBuilder(speaker, post) {
+	var pg = document.createElement('p');
+	pg.innerText = `${post}`;
+	speaker.appendChild(pg);
+}
+
+function unnecessaryDiscomfort() {
+	var ellipses = 0;
+	myVar = setInterval(function(){
+		postBuilder(therapist, "... ");
+		ellipses++;
+		if (ellipses >= 5) {
+			window.clearInterval(myVar);
+			postBuilder(therapist, 'Interesting.');
+	  }
+	}, 1000);
+}
+
 function therapistResponse() {
-	let responses = ["Mm. Uh-huh. And how does that make you feel?", "What I hear you saying is exactly what you just said", "I'm not sure that's healthy"]
 	setTimeout(function() {
-		let responses = ["Mm. Uh-huh. And how does that make you feel?", `What I hear you saying is "${patientInput.value}"`, "I'm not sure that's healthy"];
-		let p = document.createElement('p');
-		p.innerText = `${responses[Math.floor(Math.random()*3)]}`;
-		therapist.appendChild(p);
+		let responses = [
+			"How does that make you feel?",
+			`What I hear you saying is "${patientInput.value}"`,
+			"I'm not sure that's healthy",
+			"Have you considered just not feeling that way?",
+			unnecessaryDiscomfort
+		];
+		let i = Math.floor(Math.random()*responses.length)
+		console.log(i)
+
+		if (i == 4) {
+			var randResponse = responses[4]()
+		} else {
+			var randResponse = `${responses[i]}`
+		}
+		postBuilder(therapist, randResponse);
 	}, 1000);
 }
 
 patientSubmit.addEventListener('click', function() {
 	event.preventDefault();
-	let p = document.createElement('p');
-	p.innerText = `${patientInput.value}`;
-	patient.appendChild(p);
-	therapistResponse();
+	postBuilder(patient, patientInput.value)
+	preParse(patientInput.value);
 })
